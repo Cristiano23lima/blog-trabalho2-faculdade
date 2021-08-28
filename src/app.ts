ecf9398adfa3aaffcb10526b36b22e5ec68cdcc6
Cristiano Rodrigues de Lima <cristiano.lima@dellead.com>
2021-08-27 22:22:14 -0300

    feature: ativando o cors

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,37 @@
+import { NextFunction, Response } from "express";
 
 const express = require("express");
 const routes = require('./routes');
+const cors = require('cors');
+
 
 require("dotenv-safe").config();
 
 
-class App{
+class App {
     server: any = null;
 
-    constructor(){
+    constructor() {
         this.server = express();
+        this.ativarCors();
         this.middlewares();
         this.routes();
     }
 
-    routes(){
+    ativarCors() {
+        this.server.use((req: Request, res: Response, next: NextFunction) => {
+            req.headers.set("Access-Control-Allow-Origin", "*");
+            req.headers.set("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+            cors();
+            next();
+        });
+    }
+
+    routes() {
         this.server.use(routes);
     }
 
-    middlewares(){
+    middlewares() {
         this.server.use(express.json());
     }
 }
